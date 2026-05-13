4bc1febf9b8fde4b8d95be98eee915096c321a75
Eric <etouch@live.cn>
2026-05-13 14:43:07 +0800

    增加 tracing 日志的 OTLP 导出功能

--- bank/kafka-projector/src/main.rs
+++ bank/kafka-projector/src/main.rs
@@ -1,15 +1,45 @@
 use axum::{Router, http::StatusCode, routing::get};
+use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
+use opentelemetry_otlp::LogExporter;
+use opentelemetry_sdk::{Resource, logs::SdkLoggerProvider};
+use std::sync::OnceLock;
 use tracing_appender::non_blocking;
-use tracing_subscriber::fmt;
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt, util::SubscriberInitExt};
 use unis_kafka::projector::{self, Topic};
+
+fn get_resource() -> Resource {
+    static RESOURCE: OnceLock<Resource> = OnceLock::new();
+    RESOURCE
+        .get_or_init(|| {
+            Resource::builder()
+                .with_service_name("bank-projector")
+                .build()
+        })
+        .clone()
+}
+
+fn init_logs() -> SdkLoggerProvider {
+    let exporter = LogExporter::builder().build().expect("创建日志导出器失败");
+    SdkLoggerProvider::builder()
+        .with_resource(get_resource())
+        .with_batch_exporter(exporter)
+        .build()
+}
 
 #[tokio::main]
 async fn main() {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let (non_blocking, _guard) = non_blocking(std::io::stdout());
-    fmt()
+    let fmt_layer = tracing_subscriber::fmt::layer()
         .with_writer(non_blocking)
         .with_target(false)
-        .pretty()
+        .pretty();
+    let logger_provider = init_logs();
+    let otel_layer = OpenTelemetryTracingBridge::new(&logger_provider);
+    Registry::default()
+        .with(env_filter)
+        .with(fmt_layer)
+        .with(otel_layer)
         .init();
 
     let app = Router::new().route("/health", get(|| async { StatusCode::OK }));
